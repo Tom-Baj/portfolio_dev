33e1a9bf4872243c74fd7ed157ec14ce812d018b
Tom-Baj <bajouxtom@gmail.com>
2025-05-10 18:41:38 +0200

    update

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -1,35 +1,87 @@
-import baffle from "baffle";
+import { gsap } from 'gsap';
 
-/* Loader */
-window.addEventListener("load", () => {
-  const loader = document.getElementById("loader");
-  if (loader) {
-    loader.style.opacity = "0";
-    setTimeout(() => {
-      loader.style.display = "none";
+import { ScrambleTextPlugin } from 'gsap/ScrambleTextPlugin';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { SplitText } from 'gsap/SplitText';
 
-      const title = document.querySelector(".hero_title");
-      const subtitle = document.querySelector(".hero__subtitle");
-      const cta = document.querySelector(".hero__cta");
+gsap.registerPlugin(ScrambleTextPlugin, ScrollTrigger, SplitText);
 
-      if (title) title.classList.add("fade-in-up");
-      if (subtitle) subtitle.classList.add("fade-in-up");
-      if (cta) cta.classList.add("fade-in-up");
-    }, 500); // attendre la transition
+export function scrambleTitle() {
+  gsap.registerEffect({
+    name: 'scrambleSplit',
+    plugins: 'scrambleText',
+    defaults: { scrambleText: '{original}' },
+    extendTimeline: true,
+    effect(splits, vars) {
+      let tl = gsap.timeline();
+      splits.forEach((split) => {
+        let proxy = document.createElement('div'),
+          chars = split.chars,
+          l = chars.length;
+        proxy.innerText = chars.map((e) => e.innerText).join('');
+        tl.add(
+          gsap.to(proxy, vars).eventCallback('onUpdate', () => {
+            let i = l;
+            while (i--) {
+              chars[i].innerText = proxy.innerText.charAt(i);
+            }
+          }),
+          0
+        );
+      });
+      return tl;
+    },
+  });
+
+  let split = SplitText.create('.split', { type: 'chars' });
+  gsap.set('.split', { opacity: 1, delay: 0.4 });
+  gsap.effects.scrambleSplit(split, { duration: 4, delay: 0.4 });
+  gsap.from(split.chars, {
+    duration: 0.5,
+    y: 'random(-100, 100)',
+    x: 'random(-50, 50)',
+    stagger: 0.05,
+  });
+}
+
+/* gsap.registerEffect({
+	name: "scrambleSplit",
+	plugins:"scrambleText",
+	defaults: {scrambleText: "{original}"},
+	extendTimeline: true,
+	effect(splits, vars) {
+		let tl = gsap.timeline();
+		splits.forEach(split => {
+			let proxy = document.createElement("div"),
+				chars = split.chars,
+				l = chars.length;
+			proxy.innerText = split.chars.map(e => e.innerText).join("");
+			tl.add(gsap.to(proxy, vars).eventCallback("onUpdate", () => {
+					let i = l;
+					while (i--) {
+						chars[i].innerText = proxy.innerText.charAt(i);
+					}
+				}), 0);
+		});
+		return tl;
+	}
+}); */
+
+/* document.addEventListener("DOMContentLoaded", () => {
+  const title = document.querySelector(".hero_title.scramble");
+  if (title) {
+    gsap.fromTo(
+      title,
+      { scrambleText: { text: "##########", chars: "█▓▒░<>/|", revealDelay: 0.5 } },
+      {
+        duration: 2,
+        scrambleText: {
+          text: "Bonjour, je suis Tom Bajoux",
+          chars: "█▓▒░<>/|",
+          revealDelay: 0.5,
+        },
+        ease: "power2.out",
+      }
+    );
   }
-});
-
-/* Scramble */
-document.addEventListener("DOMContentLoaded", () => {
-  const text = document.querySelector(".scramble");
-
-  if (text) {
-    const effect = baffle(text);
-    effect.set({
-      characters: "█▓▒░▒▓█ ░<>/|",
-      speed: 70,
-    });
-    effect.start();
-    effect.reveal(5000);
-  }
-});
+}); */
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
-import "./animations.js";
+import { scrambleTitle } from './animations.js';
+
+const loader = document.getElementById('loader');
+const header = document.querySelector('.header');
+const main = document.querySelector('.main');
+
+/* Loader */
+window.addEventListener('DOMContentLoaded', () => {
+  if (header) header.classList.add('hidden');
+  if (main) main.classList.add('hidden');
+
+  // Simuler la durée de l'animation du loader (2.5s ici)
+  setTimeout(() => {
+    if (loader) {
+      loader.classList.add('fade-out');
+      setTimeout(() => {
+        loader.style.display = 'none';
+        if (header) header.classList.remove('hidden');
+        if (main) main.classList.remove('hidden');
+        scrambleTitle();
+      }, 500); // correspond à la transition CSS du loader
+    }
+  }, 2500);
+});
