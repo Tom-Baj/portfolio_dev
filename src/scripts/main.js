b9c7ceb21ef37c6a44026ad31ad11b2446ea0445
Tom-Baj <bajouxtom@gmail.com>
2025-05-23 02:06:51 +0200

    heder update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,13 @@ const loader = document.getElementById('loader');
 const header = document.querySelector('.header');
 const main = document.querySelector('.main');
 
+/* Scroll */
+window.addEventListener('scroll', () => {
+  console.log(window.scrollY);
+});
+
 /* Loader */
-window.addEventListener('DOMContentLoaded', () => {
+/* window.addEventListener('DOMContentLoaded', () => {
   if (header) header.classList.add('hidden');
   if (main) main.classList.add('hidden');
 
@@ -15,10 +20,20 @@ window.addEventListener('DOMContentLoaded', () => {
       loader.classList.add('fade-out');
       setTimeout(() => {
         loader.style.display = 'none';
-        if (header) header.classList.remove('hidden');
+        //if (header) header.classList.remove('hidden');
         if (main) main.classList.remove('hidden');
         scrambleTitle();
       }, 500); // correspond à la transition CSS du loader
     }
   }, 2500);
+}); */
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY === 0) {
+    header.classList.add('hidden');
+    console.log('hidden');
+  } else {
+    header.classList.remove('hidden');
+    console.log('visible');
+  }
 });
